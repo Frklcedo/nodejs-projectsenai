acf27f74319e411cf52422685185ad9f42aa1d4d
Frklcedo <franklinmacedo5115@gmail.com>
2021-12-15 22:08:58 -0300

    setup of main backend feature

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var { engine } = require('express-handlebars');
+var bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(urlencoded({ extended: false }));
+
