5d7ce9654b1d6d077927e6e85713efa05d8938d9
Frklcedo <franklinmacedo5115@gmail.com>
2021-12-15 23:54:51 -0300

    insertion of bootstrap

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.set('views', './views');
 app.use(bodyParser.json());
 app.use(urlencoded({ extended: false }));
 
+// get and post
+app.get('/', function(request,response){
+    response.render('index');
+});
+
+
+// port
+app.listen(3000);
