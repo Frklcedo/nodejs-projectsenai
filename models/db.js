c3e6a109e559bd1bbc0565ae6986e5710a2179e3
Franklin Macedo <82075583+Frklcedo@users.noreply.github.com>
2021-12-19 20:57:35 -0300

    database creation

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 // new Sequelize(banco de dados, login, senha)
 // É necessário criar banco de dados >> usersdciacat
-const sequelize = new Sequelize('usersdciacat', 'frkl', 'Frkl.db',{
+const sequelize = new Sequelize('usersdciacat', '', '',{
     host: 'localhost',
     dialect: 'mysql'
 }); 
